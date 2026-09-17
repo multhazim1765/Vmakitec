c1916744605c1720e9898943e3eee4ff5a7a2cd6
Ahamed Multhazim <ahamedmulthazim@gmail.com>
2026-09-17 15:37:04 +0530

    Update maintenance mode driver to cache with array store

--- config/app.php
+++ config/app.php
@@ -119,7 +119,7 @@ return [
     */
 
     'maintenance' => [
-        'driver' => env('APP_MAINTENANCE_DRIVER', 'array'),
+        'driver' => env('APP_MAINTENANCE_DRIVER', 'cache'),
         'store' => env('APP_MAINTENANCE_STORE', 'array'),
     ],
 
